2cd50376f8f423c6bba0808d8b1cdad540bdd59d
Gabriel Silva de Rezende <802.239@alunos.unigran.br>
2022-10-18 14:40:05 -0400

    Add código nas Activity

--- app/src/main/java/com/gabriel/appdecadastro/MainActivity.java
+++ app/src/main/java/com/gabriel/appdecadastro/MainActivity.java
@@ -2,13 +2,69 @@ package com.gabriel.appdecadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botaoProdutos;
+    Button botaoClientes;
+    Button botaoFornecedores;
+    Button botaoSair;
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoProdutos = findViewById(R.id.botaoProdutos);
+        botaoClientes = findViewById(R.id.botaoClientes);
+        botaoFornecedores = findViewById(R.id.botaoFornecedores);
+        botaoSair = findViewById(R.id.botaoSair);
+
+        botaoProdutos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, SegundaActivity.class);
+
+                intent.putExtra("opcao", 0);
+
+                startActivity(intent);
+            }
+        });
+
+        botaoClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, SegundaActivity.class);
+
+                intent.putExtra("opcao", 1);
+
+                startActivity(intent);
+            }
+        });
+
+        botaoFornecedores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, SegundaActivity.class);
+
+                intent.putExtra("opcao", 2);
+
+                startActivity(intent);
+            }
+        });
+
+        botaoSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/gabriel/appdecadastro/SegundaActivity.java
+++ app/src/main/java/com/gabriel/appdecadastro/SegundaActivity.java
@@ -1,14 +1,96 @@
 package com.gabriel.appdecadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SegundaActivity extends AppCompatActivity {
+
+    Button botaoListagem;
+    Button botaoCadastrar;
+
+    Integer opcao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        opcao = getIntent().getIntExtra("opcao", -1);
+
+        if (opcao == -1) {
+            onBackPressed();
+        } else {
+            botaoListagem = findViewById(R.id.botaoListagem);
+            botaoCadastrar = findViewById(R.id.botaoCadastrar);
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            switch (opcao) {
+                case 0: {
+                    transaction.add(R.id.frameSegundaActivity, new ListaProdutoFragment());
+                    break;
+                }
+
+                case 1: {
+                    transaction.add(R.id.frameSegundaActivity, new ListaClienteFragment());
+                    break;
+                }
+
+                case 2: {
+                    transaction.add(R.id.frameSegundaActivity, new ListaFornecedorFragment());
+                    break;
+                }
+            }
+            transaction.commit();
+
+            botaoListagem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    switch (opcao) {
+                        case 0: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.frameSegundaActivity, new ListaProdutoFragment()).commit();
+                            break;
+                        }
+
+                        case 1: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.frameSegundaActivity, new ListaClienteFragment()).commit();
+                            break;
+                        }
+
+                        case 2: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.frameSegundaActivity, new ListaFornecedorFragment()).commit();
+                            break;
+                        }
+                    }
+                }
+            });
+
+            botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    switch (opcao) {
+                        case 0: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.frameSegundaActivity, new CadastroProdutoFragment()).commit();
+                            break;
+                        }
+
+                        case 1: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.frameSegundaActivity, new CadastroClienteFragment()).commit();
+                            break;
+                        }
+
+                        case 2: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.frameSegundaActivity, new CadastroFornecedorFragment()).commit();
+                            break;
+                        }
+                    }
+                }
+            });
+
+        }
     }
 }
